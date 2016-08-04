3f9fe18021351f2a5fb6615ffaba00997eb25a11
nbkhope <ryumemaru@yahoo.com>
2016-08-04 15:51:53 -0700

    Remove unnecessary styles at the end and add backgroundImage property style; Add image to the background of Home component

--- SnowSchoolers/Home.js
+++ SnowSchoolers/Home.js
@@ -4,6 +4,7 @@ import {
   Text,
   View,
   TouchableOpacity,
+  Image,
 } from 'react-native';
 
 class Home extends Component {
@@ -21,52 +22,50 @@ class Home extends Component {
   render() {
     return (
       <View style={this.props.style.container}>
-        <TouchableOpacity onPress={this._onPressBookLesson.bind(this)}>
-          <Text>
-            Book Lesson
+        <Image source={require('./images/tetons.jpg')} style={styles.backgroundImage}>
+          <Text style={this.props.style.welcome}>
+            Welcome to SnowSchoolers!
           </Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => this._navigate('updatelesson')}>
-          <Text>
-            Update Lesson
+          <Text style={this.props.style.instructions}>
+            Experience the Snowies with personalized lessons
           </Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => this._navigate('lessondetails')}>
-          <Text>
-            Lesson Details
-          </Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={this._onPressBookLesson.bind(this)}>
+            <Text>
+              Book Lesson
+            </Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={this._onPressNotFound.bind(this)}>
-          <Text>
-            Not Found
-          </Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => this._navigate('updatelesson')}>
+            <Text>
+              Update Lesson
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => this._navigate('lessondetails')}>
+            <Text>
+              Lesson Details
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={this._onPressNotFound.bind(this)}>
+            <Text>
+              Not Found
+            </Text>
+          </TouchableOpacity>
+        </Image>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: 'blue',
-    fontWeight: 'bold',
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    //resizeMode: 'cover',
   },
 });
 
