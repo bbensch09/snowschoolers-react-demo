6b1b1d1540e0335e8bd280bf7ca4b9ce3e708c71
nbkhope <ryumemaru@yahoo.com>
2016-08-12 16:56:23 -0700

    Change container style to stretch alignItems

--- SnowSchoolers/index.ios.js
+++ SnowSchoolers/index.ios.js
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
     //backgroundColor: '#F5FCFF',
   },
   // home page welcome message
